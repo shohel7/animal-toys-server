c7fe023731754a4168ffae4af2a5e0ee5ac27c9e
shohel-ranaC <akashshohel422@gmail.com>
2023-05-19 22:13:41 -0700

    created single toy api by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -33,6 +33,12 @@ async function run() {
 
     app.get("/allToys", async (req, res) => {
       const result = await animalToysCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await animalToysCollection.findOne(query);
       res.send(result);
     });
 
